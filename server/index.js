31956ac0265ac7851a40819bf7d5a84ff70d6b86
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-07-24 11:06:11 -0700

    third commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const massive = require('massive');
 const bodyParser = require('body-parser');
+const path = require('path')
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use( express.static( `${__dirname}/../build` ) );
+
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 PORT = 4000;
 app.listen(PORT, () => console.log('Server is listening on port ' + PORT + ' 🚀'))
