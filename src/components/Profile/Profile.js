42c409102332e276597589ddfbf72c611f9c2a10
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-08-08 16:55:15 -0700

    Add responsive profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -90,59 +90,61 @@ class Profile extends Component {
                 <div className='profile_container'>
                 <h1>Profile</h1>
                 <br/>
-                <h3>{username}</h3>
-                <img src={profilePicture} alt="profile from login information"/>
-                <br/>
-                <h3>Pick a Meal Plan</h3>
-                <select onChange={e => this.selectTitleIdFunc(e.target.value)}>
-                <option value={0} defaultValue>Select One</option>
-                {displayTitles}
-                </select>
-                <br/>
-                <button onClick={() => this.fetchMealplan()}>Fetch Meal Plan</button>
-                <br/>
-                <Link to='EditMealplan'><button style={{width: '100%'}}onClick={() => this.editMealplan()}>Edit Meal Plan</button></Link>
-                <br/>
-                <button onClick={() => this.deleteMealplan()}>Delete Meal Plan</button>
-                <br/>
-                <div className='mealplanContainer'>
-                <div className='section'>
-                <h3>Meal List(per 100g)</h3>
-                {displayMealList}
-                </div>
-                <br/>
-                <div className='section'>
-                <h3>Total Count</h3>
-                <p>Fat: {this.state.mealplans ? (+this.state.mealplans[0].total_fat).toFixed(2) : 0}g</p> 
-                <p>Carbs: {this.state.mealplans ? (+this.state.mealplans[0].total_carbohydrate).toFixed(2) : 0}g</p>
-                <p>Protein: {this.state.mealplans ? (+this.state.mealplans[0].total_protein).toFixed(2) : 0}g</p>
-                <p>Calories: {this.state.mealplans ? (+this.state.mealplans[0].total_calories).toFixed(2) : 0}kcal</p> 
-                <p>Vitamin A: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_a).toFixed(2) : 0}µg</p> 
-                <p>Vitamin C: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_c).toFixed(2) : 0}mg</p> 
-                <p>Vitamin D: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_d).toFixed(2) : 0}IU</p> 
-                <p>Vitamin E: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_e).toFixed(2) : 0}mg</p> 
-                <p>Vitamin K: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_k).toFixed(2) : 0}µg</p> 
-                <p>Thiamin: {this.state.mealplans ? (+this.state.mealplans[0].total_thiamin).toFixed(2) : 0}mg</p> 
-                <p>Riboflavin: {this.state.mealplans ? (+this.state.mealplans[0].total_riboflavin).toFixed(2) : 0}mg</p> 
-                <p>Niacin: {this.state.mealplans ? (+this.state.mealplans[0].total_niacin).toFixed(2) : 0}mg</p> 
-                <p>Vitamin B6: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_b6).toFixed(2) : 0}mg</p> 
-                <p>Biotin: {this.state.mealplans ? (+this.state.mealplans[0].total_biotin).toFixed(2) : 0}IU</p> 
-                <p>Folate: {this.state.mealplans ? (+this.state.mealplans[0].total_folate).toFixed(2) : 0}µg</p> 
-                <p>Vitamin B12: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_b12).toFixed(2) : 0}µg</p> 
-                <p>Calcium: {this.state.mealplans ? (+this.state.mealplans[0].total_calcium).toFixed(2) : 0}mg</p> 
-                <p>Copper: {this.state.mealplans ? (+this.state.mealplans[0].total_copper).toFixed(2) : 0}mg</p> 
-                <p>Fluoride: {this.state.mealplans ? (+this.state.mealplans[0].total_fluoride).toFixed(2) : 0}µg</p> 
-                <p>Iodine: {this.state.mealplans ? (+this.state.mealplans[0].total_iodine).toFixed(2) : 0}IU</p> 
-                <p>Iron: {this.state.mealplans ? (+this.state.mealplans[0].total_iron).toFixed(2) : 0}mg</p> 
-                <p>Magnesium: {this.state.mealplans ? (+this.state.mealplans[0].total_magnesium).toFixed(2) : 0}mg</p> 
-                <p>Manganese: {this.state.mealplans ? (+this.state.mealplans[0].total_manganese).toFixed(2) : 0}mg</p> 
-                <p>Phosphorus: {this.state.mealplans ? (+this.state.mealplans[0].total_phosphorus).toFixed(2) : 0}mg</p> 
-                <p>Potassium: {this.state.mealplans ? (+this.state.mealplans[0].total_potassium).toFixed(2) : 0}mg</p> 
-                <p>Sodium: {this.state.mealplans ? (+this.state.mealplans[0].total_sodium).toFixed(2) : 0}mg</p> 
-                <p>Selenium: {this.state.mealplans ? (+this.state.mealplans[0].total_selenium).toFixed(2) : 0}µg</p> 
-                <p>Zinc: {this.state.mealplans ? (+this.state.mealplans[0].total_zinc).toFixed(2) : 0}mg</p> 
-                </div>
-                </div>
+                    <div className='profile_row'>
+                        <div className='profile_user_section'>
+                        <h3>{username}</h3>
+                        <img src={profilePicture} alt="profile from login information"/>
+                        <br/>
+                        <h3>Pick a Meal Plan</h3>
+                        <select onChange={e => this.selectTitleIdFunc(e.target.value)}>
+                        <option value={0} defaultValue>Select One</option>
+                        {displayTitles}
+                        </select>
+                        <br/>
+                        <button onClick={() => this.fetchMealplan()}>Fetch Meal Plan</button>
+                        <br/>
+                        <Link to='EditMealplan'><button onClick={() => this.editMealplan()}>Edit Meal Plan</button></Link>
+                        <br/>
+                        <button onClick={() => this.deleteMealplan()}>Delete Meal Plan</button>
+                        <br/>
+                        </div>
+                    <div className='profile_section'>
+                    <h3>Meal List(per 100g)</h3>
+                    {displayMealList}
+                    </div>
+                    <br/>
+                    <div className='profile_section'>
+                    <h3>Total Count</h3>
+                    <p>Fat: {this.state.mealplans ? (+this.state.mealplans[0].total_fat).toFixed(2) : 0}g</p> 
+                    <p>Carbs: {this.state.mealplans ? (+this.state.mealplans[0].total_carbohydrate).toFixed(2) : 0}g</p>
+                    <p>Protein: {this.state.mealplans ? (+this.state.mealplans[0].total_protein).toFixed(2) : 0}g</p>
+                    <p>Calories: {this.state.mealplans ? (+this.state.mealplans[0].total_calories).toFixed(2) : 0}kcal</p> 
+                    <p>Vitamin A: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_a).toFixed(2) : 0}µg</p> 
+                    <p>Vitamin C: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_c).toFixed(2) : 0}mg</p> 
+                    <p>Vitamin D: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_d).toFixed(2) : 0}IU</p> 
+                    <p>Vitamin E: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_e).toFixed(2) : 0}mg</p> 
+                    <p>Vitamin K: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_k).toFixed(2) : 0}µg</p> 
+                    <p>Thiamin: {this.state.mealplans ? (+this.state.mealplans[0].total_thiamin).toFixed(2) : 0}mg</p> 
+                    <p>Riboflavin: {this.state.mealplans ? (+this.state.mealplans[0].total_riboflavin).toFixed(2) : 0}mg</p> 
+                    <p>Niacin: {this.state.mealplans ? (+this.state.mealplans[0].total_niacin).toFixed(2) : 0}mg</p> 
+                    <p>Vitamin B6: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_b6).toFixed(2) : 0}mg</p> 
+                    <p>Biotin: {this.state.mealplans ? (+this.state.mealplans[0].total_biotin).toFixed(2) : 0}IU</p> 
+                    <p>Folate: {this.state.mealplans ? (+this.state.mealplans[0].total_folate).toFixed(2) : 0}µg</p> 
+                    <p>Vitamin B12: {this.state.mealplans ? (+this.state.mealplans[0].total_vitamin_b12).toFixed(2) : 0}µg</p> 
+                    <p>Calcium: {this.state.mealplans ? (+this.state.mealplans[0].total_calcium).toFixed(2) : 0}mg</p> 
+                    <p>Copper: {this.state.mealplans ? (+this.state.mealplans[0].total_copper).toFixed(2) : 0}mg</p> 
+                    <p>Fluoride: {this.state.mealplans ? (+this.state.mealplans[0].total_fluoride).toFixed(2) : 0}µg</p> 
+                    <p>Iodine: {this.state.mealplans ? (+this.state.mealplans[0].total_iodine).toFixed(2) : 0}IU</p> 
+                    <p>Iron: {this.state.mealplans ? (+this.state.mealplans[0].total_iron).toFixed(2) : 0}mg</p> 
+                    <p>Magnesium: {this.state.mealplans ? (+this.state.mealplans[0].total_magnesium).toFixed(2) : 0}mg</p> 
+                    <p>Manganese: {this.state.mealplans ? (+this.state.mealplans[0].total_manganese).toFixed(2) : 0}mg</p> 
+                    <p>Phosphorus: {this.state.mealplans ? (+this.state.mealplans[0].total_phosphorus).toFixed(2) : 0}mg</p> 
+                    <p>Potassium: {this.state.mealplans ? (+this.state.mealplans[0].total_potassium).toFixed(2) : 0}mg</p> 
+                    <p>Sodium: {this.state.mealplans ? (+this.state.mealplans[0].total_sodium).toFixed(2) : 0}mg</p> 
+                    <p>Selenium: {this.state.mealplans ? (+this.state.mealplans[0].total_selenium).toFixed(2) : 0}µg</p> 
+                    <p>Zinc: {this.state.mealplans ? (+this.state.mealplans[0].total_zinc).toFixed(2) : 0}mg</p> 
+                    </div>
+                    </div>
                 </div>
                 :
                 <Auth/>
